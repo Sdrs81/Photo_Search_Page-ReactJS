86cda39b8de000fb6bffe93303075c3f4cd2a8c7
Sdrs81 <serdardemirtas25@gmail.com>
2023-03-19 15:11:49 +0300

    Images are now displayed and CSS adjustments have been made

--- src/Components/ImageItem.js
+++ src/Components/ImageItem.js
@@ -1,7 +1,6 @@
 function ImageItem({image}) {
-    console.log(image)
     return ( <div>
-        
+        <img className="imageListImg" src={image.urls.small} alt={image.alt_description} />
     </div> );
 }
 
--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,7 +1,7 @@
 import ImageItem from "./ImageItem";
 
 function ImageList({imagesPlaceHolder}) {
-    return ( <div>
+    return ( <div className="imageList">
         {imagesPlaceHolder.map((image, index) =>{
            return <ImageItem key={index} image={image}/>
         })}
